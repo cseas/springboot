611d744d63d894c7a712ae9e82e4b6191b76993d
cseas <contact.abhijeetsingh@gmail.com>
2021-01-30 00:35:30 +0530

    Find beans used by Spring

--- src/test/java/com/absingh/demo/DemoApplicationTests.java
+++ src/test/java/com/absingh/demo/DemoApplicationTests.java
@@ -1,13 +1,26 @@
 package com.absingh.demo;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
-class DemoApplicationTests {
+public class DemoApplicationTests {
+	@Autowired
+	private ApplicationContext ctx;
 
 	@Test
 	void contextLoads() {
+		int count = ctx.getBeanDefinitionCount();
+		System.out.println("There are " + count + " beans in the application context");
+
+		for(String name : ctx.getBeanDefinitionNames()) {
+			System.out.println(name);
+		}
 	}
 
 }
