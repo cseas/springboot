7c57080132dae60bb0b17fa76289bf6efa7f116d
cseas <contact.abhijeetsingh@gmail.com>
2021-01-30 01:44:10 +0530

    Add custom Bean

--- src/main/java/com/absingh/demo/DemoApplication.java
+++ src/main/java/com/absingh/demo/DemoApplication.java
@@ -2,10 +2,18 @@ package com.absingh.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.text.NumberFormat;
 
 @SpringBootApplication
 public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Bean
+	public NumberFormat defaultCurrencyFormat() {
+		return NumberFormat.getCurrencyInstance();
 	}
 }
