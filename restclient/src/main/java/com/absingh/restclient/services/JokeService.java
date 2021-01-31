b5a133ff14fe9cb439ce1a6b2cb21b8baeec0ed8
cseas <contact.abhijeetsingh@gmail.com>
2021-01-31 17:31:32 +0530

    Create Simple Rest Get service

--- restclient/src/main/java/com/absingh/restclient/services/JokeService.java
+++ restclient/src/main/java/com/absingh/restclient/services/JokeService.java
@@ -1,5 +1,6 @@
 package com.absingh.restclient.services;
 
+import com.absingh.restclient.json.JokeResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Service;
@@ -7,12 +8,24 @@ import org.springframework.web.client.RestTemplate;
 
 @Service
 public class JokeService {
+  private static final String BASE = "http://api.icndb.com/jokes/random?limitTo=[nerdy]";
 
   private RestTemplate restTemplate;
 
   @Autowired
   public JokeService(RestTemplateBuilder builder) {
     restTemplate = builder.build();
+  }
+
+  public String getJoke(String firstName, String lastName) {
+    String url = String.format("%s&firstName=%s&lastName=%s", BASE, firstName, lastName);
+    JokeResponse response = restTemplate.getForObject(url, JokeResponse.class);
+
+    String output = "";
+    if (response != null) {
+    output = response.getValue().getJoke();
+    }
+    return output;
   }
 }
 
